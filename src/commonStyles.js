2d3c328164cc0cfde5e1c5b9b761cbf6ac21771a
IgorCS <igorweb08@gmail.com>
2019-07-17 22:30:28 -0300

    assync

---
+++ src/commonStyles.js
@@ -0,0 +1,19 @@
+export default {
+    fontFamily: 'Lato',
+    //colors: {
+        //today: '#b13b44',
+        //secondary: '#FFF',
+       // mainText: '#222',
+        //subText: '#555',
+    //}
+    colors: {
+        default: '#1631be',
+        today: '#b13b44',
+        tomorrow: '#c9742e',
+        week: '#15721e',
+        month: '#1631be',
+        secondary: '#FFF',
+        mainText: '#222',
+        subText: '#555',
+    }
+}
